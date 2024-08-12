91e1ec2ee850118cde6b6a547bf6b3209f839fb4
diego <daazurdiao@correo.url.edu.gt>
2024-08-12 02:37:04 -0600

    creacion de los archivos

---
+++ ProyectoConsola/ProyectoConsola/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProyectoConsola.FilesManagers;
+
+FileManager fileManager = new FileManager();
+
+Dictionary<string, List<string>> seccionesProcesadas = fileManager.ProcesarArchivo("GRMAR.txt"); // \Proyecto\ProyectoConsola\ProyectoConsola\bin\Debug\net8.0\GRMAR.txt
+//Imprimir secciones
+foreach (var seccion in seccionesProcesadas)
+{
+    Console.WriteLine($"Sección: {seccion.Key}");
+    foreach (var valor in seccion.Value)
+    {
+        Console.WriteLine($"  - {valor}");
+    }
+}
