e97270a8ac7f774b522e7737a6c4ecd525235e89
diego <daazurdiao@correo.url.edu.gt>
2024-11-05 01:47:01 -0600

    definicion de acciones, falta implementarlas

--- ProyectoConsola/ProyectoConsola/Managers/LALRParser.cs
+++ ProyectoConsola/ProyectoConsola/Managers/LALRParser.cs
@@ -10,20 +10,23 @@ using ProyectoConsola.Managers;
 using ProyectoConsola.Estructuras;
 using static System.Collections.Specialized.BitVector32;
 using static OfficeOpenXml.ExcelErrorValue;
+using System.Collections.ObjectModel;
 
 namespace ProyectoConsola.Parsing
 {
     public class LALRParser
     {
-        private readonly Dictionary<int, Dictionary<object, (LALRAction, int)>> _actionTable;
+        private readonly LALRTableManager _lALRTableManager;
+        private readonly Dictionary<int, Dictionary<object, (LALRAction, int)>> _actionTable
         private readonly string _startSymbol;
         SectionsManager _sectionsManager;
 
 
-        public LALRParser(Dictionary<int, Dictionary<object, (LALRAction, int)>> actionTable, string startSymbol, SectionsManager sm)
+        public LALRParser(LALRTableManager lALRTableManager, SectionsManager sm)
         {
-            _actionTable = actionTable;
-            _startSymbol = startSymbol;
+            _lALRTableManager = lALRTableManager;
+            _actionTable = lALRTableManager._actionTable;
+            _startSymbol = sm._startSymbol;
             _sectionsManager = sm;
         }
         private List<(string identifier, string production)> RearrangeTokens(List<Token> tuplas)
@@ -106,7 +109,8 @@ namespace ProyectoConsola.Parsing
                         {
                             stateStack.Pop(); // Remueve el estado
                             (string symbol, string value) symbol_Value = symbolStack.Pop(); // Remueve el simbolo
-                            if (currentSymbol.Equals(symbol_Value.symbol))
+                            string trim = _lALRTableManager.TrimSymbol(symbol);
+                            if (trim.Equals(symbol_Value.symbol))
                             {
                                 values.Add(symbol_Value.value);
                             }
@@ -140,27 +144,7 @@ namespace ProyectoConsola.Parsing
                     return false;
             }
         }
-        private string DoSyntaxActions(string nonTerminal,string production,List<string> values)
-        {
-            string result = "";
-            List<string> actions ;
-            var semanticActionsDictionary = _sectionsManager._nonTerminalActions;
-            if (semanticActionsDictionary.Keys.Contains(nonTerminal) && semanticActionsDictionary[nonTerminal].Keys.Contains(production))
-            {
-                actions = _sectionsManager._nonTerminalActions[nonTerminal][production];
-                foreach (var action in actions)
-                {
-                    switch (action)
-                    {
-                        
-                        default:
-                            // Manejo de caso por defecto
-                            break;
-                    }
-                }
-            }
-            return String.Join(" ,", values);
-        }
+        
         public bool Parse(string inputString)
         {
 
@@ -229,5 +213,184 @@ namespace ProyectoConsola.Parsing
                 }
             }
         }
+        private string DoSyntaxActions(string nonTerminal, string production, List<string> values)
+        {
+            string result = "";
+            List<string> actions;
+            var semanticActionsDictionary = _sectionsManager._nonTerminalActions;
+            if (semanticActionsDictionary.Keys.Contains(nonTerminal) && semanticActionsDictionary[nonTerminal].Keys.Contains(production))
+            {
+                actions = _sectionsManager._nonTerminalActions[nonTerminal][production];
+                foreach (var action in actions)
+                {
+                    switch (action)
+                    {
+                        case "save_program":
+                            // 5
+
+                            break;
+                        case "save_block":
+                            // 2
+                            break;
+                        case "save_declarations":
+                            // 2
+                            break;
+                        case "keep_value":
+                            // 1
+                            break;
+                        case "save_var_declaration":
+                            // 6
+                            break;
+                        case "save_procedure_declaration":
+                            // 8
+                            break;
+                        case "save_parameter_list":
+                            // 4
+                            break;
+                        case "save_parameter_list_ext":
+                            // 5
+                            break;
+                        case "save_type":
+                            // 1
+                            break;
+                        case "save_compound_statement":
+                            // 3
+                            break;
+                        case "save_statement_list":
+                            // 2
+                            break;
+                        case "save_statement_list_ext":
+                            // 3
+                            break;
+                        case "save_statement":
+                            // 1
+                            break;
+                        case "save_assignment":
+                            // 3
+                            break;
+                        case "save_if_statement":
+                            // 5
+                            break;
+                        case "save_else_statement":
+                            // 2
+                            break;
+                        case "save_while_statement":
+                            // 4
+                            break;
+                        case "save_procedure_call":
+                            // 4
+                            break;
+                        case "save_argument_list":
+                            // 2
+                            break;
+                        case "save_argument_list_ext":
+                            // 3
+                            break;
+                        case "save_io_statement":
+                            // 4
+                            break;
+                        case "save_expression":
+                            // 2
+                            break;
+                        case "save_expression_extension":
+                            // 2
+                            break;
+                        case "save_simple_expression":
+                            // 3
+                            break;
+                        case "save_term":
+                            // 3
+                            break;
+                        case "save_factor":
+                            // 3
+                            break;
+                        case "save_identifier":
+                            // 1
+                            break;
+                        case "save_number":
+                            // 1
+                            break;
+                        case "save_operator":
+                            // 1
+                            break;
+                        case "save_string":
+                            // 3
+                            break;
+                        case "save_boolean":
+                            // 1
+                            break;
+                        default:
+                            // Manejo de caso por defecto
+                            break;
+                    }
+                }
+            }
+            return String.Join(" ", values);
+        }
+        // save_program
+        // save_block
+        // save_declarations
+        // save_declarations
+        // keep_value
+        // save_var_declaration
+        // keep_value
+        // save_procedure_declaration
+        // save_parameter_list
+        // keep_value
+        // save_parameter_list_ext
+        // save_type
+        // save_type
+        // save_type
+        // save_type
+        // save_compound_statement
+        // save_statement_list
+        // save_statement_list
+        // save_statement_list_ext
+        // keep_value
+        // save_statement
+        // save_statement
+        // save_statement
+        // save_statement
+        // save_statement
+        // save_assignment
+        // save_if_statement
+        // save_else_statement
+        // keep_value
+        // save_while_statement
+        // save_procedure_call
+        // save_argument_list
+        // keep_value
+        // save_argument_list_ext
+        // keep_value
+        // save_io_statement
+        // save_io_statement
+        // save_expression
+        // save_expression_extension
+        // keep_value
+        // save_simple_expression
+        // keep_value
+        // save_term
+        // keep_value
+        // save_factor
+        // save_identifier
+        // save_number
+        // save_operator
+        // save_string
+        // save_operator
+        // save_operator
+        // save_operator
+        // save_operator
+        // save_operator
+        // save_operator
+        // save_operator
+        // save_operator
+        // save_operator
+        // save_operator
+        // save_operator
+        // save_operator
+        // save_boolean
+        // save_boolean
+        // save_string
+
     }
 }
